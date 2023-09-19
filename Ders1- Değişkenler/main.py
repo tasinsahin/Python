7ccc88dabc8deaf8b4b1286ab196af466e2ff24d
Şahin <tasinsahin@gmail.com>
2023-09-19 15:45:23 +0000

    dersler eklendi

---
+++ Ders1- Değişkenler/main.py
@@ -0,0 +1,97 @@
+
+# ------------------------------------------------------------------
+#Yönerge:
+#print fonksiyonu ile aşağıdaki çıktıyı veren kodu yazalım
+
+#Çıktı:
+#Phyton ile ilk günüm!
+#Fonksiyon şu şekilde tanımlanır: print('ekrana yazdırılacak yazı')
+
+#Uygulama:
+print('Phyton ile ilk günüm!')
+#Hatalı Uygulama:
+#print("Fonksiyon şu şekilde tanımlanır: print("ekrana yazdırılacak yazı")")
+#Hatalı Uygulama:
+#print('Phyton ile ilk günüm!) #Tırnak kapatılmamış
+print("Fonksiyon şu şekilde tanımlanır: print('ekrana yazdırılacak yazı')")
+# ------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------
+#Yönerge:
+#Üç defa alt alta print fonksiyonu kullanarak Merhaba Dünya yazdıralım
+
+#Çıkı:
+#Merhaba Dünya
+#Merhaba Dünya
+#Merhaba Dünya
+
+#Uygulama:
+print("Merhaba Dünya\nMerhaba Dünya\nMerhaba Dünya\n")
+#Bir Diğer Method:
+print("Merhaba Dünya")
+print("Merhaba Dünya")
+print("Merhaba Dünya")
+# ------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------
+#Yönerge:
+#Ekrana Merhaba Şahin yazdıralım
+
+#Çıktı:
+#Merhaba Şahin
+
+#Uygulama
+print("Merhaba"+" "+"Şahin") #Araya boşluk koymalıyız
+#Hatalı Uygulama
+print("Merhaba"+"Şahin") #Araya boşluk koyulmamış
+#Hatalı Uygulama
+print("Merhaba"+"Şahin") #Araya boşluk koyulmamış
+#Hatalı Uygulama
+# print("Merhaba"+" "+"Şahin") #kodun başına boşluk konulmuş indent error verecektir.
+# ------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+
+
+
+
+
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+
+
+
