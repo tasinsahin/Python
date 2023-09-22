039c180dbef5f9d907e8e258f6a1075f0e0c366d
Şahin <tasinsahin@gmail.com>
2023-09-22 11:01:31 +0000

    gsdgfds

--- Başlangıç/Ders4 - Diziler/Ders4.1.py
+++ Başlangıç/Ders4 - Diziler/Ders4.1.py
@@ -1,5 +1,10 @@
 # ------------------------------------------------------------------
-#Yönerge:  
+#Yönerge:  Taş kağıt makas oyunu için kodu yazalım
+#https://www.askpython.com/
 #Çıktı:
 #Uygulama: 
+import random
+
+randomSayi = random.randint(1,3)
+cevap = int(input("Taş (1), Kağıt (2) Makas (3) Seçiniz"))
 # ------------------------------------------------------------------
