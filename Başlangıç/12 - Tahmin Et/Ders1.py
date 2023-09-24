d220ca3377f6225a686bffd35ca6cff183c408bb
Şahin <tasinsahin@gmail.com>
2023-09-24 10:11:08 +0000

    oniki bitirildi

---
+++ Başlangıç/12 - Tahmin Et/Ders1.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------
+#Yönerge:  
+#http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+#Çıktı:
+#Uygulama: enemies = 1
+
+def increase_enemies():
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+
+# ------------------------------------------------------------------
--- Başlangıç/12 - Tahmin Et/Ders2.py
+++ Başlangıç/12 - Tahmin Et/Ders2.py
@@ -2,4 +2,13 @@
 #Yönerge:  
 #Çıktı:
 #Uygulama: 
+
+enemies = 1
+
+def increase_enemies():
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
 # ------------------------------------------------------------------
