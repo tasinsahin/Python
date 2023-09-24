8cd9f36d7d27c3f3dca63ab05ac30ef8ed0fd97c
Şahin <tasinsahin@gmail.com>
2023-09-24 16:11:37 +0000

    14 bitti

--- Başlangıç/13 - Hata Ayıklama/Ders1.py
+++ Başlangıç/13 - Hata Ayıklama/Ders1.py
@@ -1,5 +1,6 @@
 # ------------------------------------------------------------------
 #Yönerge:  
+#https://pythontutor.com/visualize.html#mode=edit
 #Çıktı:
 #Uygulama: 
 
@@ -13,7 +14,11 @@ fonksiyon()
 
 # 2- Hatayı Tekrar Üret
 # 3- Satır Satır Gez
-# 4- Kırmızı yanan satırlar
+# 4- Kırmızı yanan satırlara dikkat et
 # 5- print() fonksiyonunu kullan
 # 6- Hata Ayıklamayı kullan
+# 7- Hatayı Çöz
+# 8- Çözemediğinde Ara Ver
+# 9- Her seferinde tek bir probleme odaklan
+# 10- Hatayı google la - Stackoverflow a Sor
 # ------------------------------------------------------------------
